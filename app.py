5c5a2866e148171a47760f1e0fd60c57d4c16c6d
soumalya1902 <149316877+soumalya1902@users.noreply.github.com>
2024-03-23 04:11:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,12 +51,6 @@ if st.button('Recommend Medicine'):
           st.write("Click here -> "+" https://pharmeasy.in/search/all?name="+i) # Recommnded-drug purchase link from pharmeasy
           j+=1
 
-                                      #Page Configuration
-st.set_page_config(
-    page_title="Recominac",
-    
-    initial_sidebar_state = 'auto'
-)
 
 
 
